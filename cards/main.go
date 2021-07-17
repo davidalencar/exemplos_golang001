542b3304d6c8a56c9eb6ba7239b942f2d4e89b58
David <davidalencarignacio@gmail.com>
2021-07-17 00:47:08 -0300

    upd

--- cards/main.go
+++ cards/main.go
@@ -6,8 +6,13 @@ func main() {
 	cards := newDeck()
 
 	hand, remainingCards := deal(cards, 3)
+
 	fmt.Println("Na minha mão")
 	hand.print()
+
 	fmt.Println("Na mesa")
 	remainingCards.print()
+
+	fmt.Println("E minhas cartas estao como?")
+	cards.print()
 }
