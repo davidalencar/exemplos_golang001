26d87fab75d493ce1bc96e174de6a9b92f77e030
David <davidalencarignacio@gmail.com>
2021-07-23 21:49:13 -0300

    Working with channels

--- channels/channels.go
+++ channels/channels.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,18 +15,30 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, l := range links {
-		go checkLink(l)
+		go checkLink(l, c)
 	}
+
+	for l := range c {
+		go func(l string) {
+			time.Sleep(time.Second)
+			checkLink(l, c)
+		}(l)
+	}
+
 }
 
-func checkLink(l string) {
+func checkLink(l string, c chan string) {
 	resp, err := http.Get(l)
 
 	if err != nil {
 		fmt.Println(l, "--> Error:", err)
+		c <- l
 		return
 	}
 
 	fmt.Println(resp.Status, l)
+	c <- l
 }
